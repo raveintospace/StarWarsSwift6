ad135d52a841ed66ea045f01278e38c44a0c3cc5
Oriol Angelet <uriaeri@gmail.com>
2024-12-30 19:25:38 +0100

    Model iteration

--- StarWarsSwift/StarWarsSwift/Model.swift
+++ StarWarsSwift/StarWarsSwift/Model.swift
@@ -1,10 +1,38 @@
 //
 //  Model.swift
 //  StarWarsSwift
-//
+//  DTO: Data Transfer Object
 //  Created by Uri on 30/12/24.
 //
 
 import Foundation
 
+// From Json to instance with codable, what we load
+struct StarCardDTO: Codable {
+    let id: Int
+    let nombre: String
+    let raza: String
+    let descripcion: String
+    let planetaOrigen: String
+    let epoca: String
+    let afiliacion: String
+    let habilidades: String
+    let armas: String
+    let imagen: String
+}
 
+// What we want to return
+// Identifiable: To enumerate elements
+// Hashable: Unique comparison value, two different elements (data - StarCard1 Vs StarCard2) with exact values inside their struct
+struct StarCard: Identifiable, Hashable {
+    let id: Int
+    let nombre: String
+    let raza: String
+    let descripcion: String
+    let planetaOrigen: String
+    let epoca: String
+    let afiliacion: [String]
+    let habilidades: [String]
+    let armas: [String]
+    let imagen: String
+}
