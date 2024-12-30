80c857da372a17a8cb313df821de9941625bd7be
Oriol Angelet <uriaeri@gmail.com>
2024-12-30 19:08:12 +0100

    Initial commit

---
+++ StarWarsSwift/StarWarsSwift/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  StarWarsSwift
+//
+//  Created by Uri on 30/12/24.
+//
+
+import Foundation
+
+
---
+++ StarWarsSwift/StarWarsSwift/StarWarsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSwiftApp.swift
+//  StarWarsSwift
+//  https://youtu.be/4JlSxaIWRJ0?si=VP7sX8LRImOlHIbp
+//  Created by Uri on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
