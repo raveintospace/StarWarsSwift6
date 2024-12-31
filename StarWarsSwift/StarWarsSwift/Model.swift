bd2567694839aa0432c6617201159efe61e6de35
Oriol Angelet <uriaeri@gmail.com>
2024-12-31 15:31:52 +0100

    Model finished after adding a mapper

--- StarWarsSwift/StarWarsSwift/Model.swift
+++ StarWarsSwift/StarWarsSwift/Model.swift
@@ -21,7 +21,24 @@ struct StarCardDTO: Codable {
     let imagen: String
 }
 
-// What we want to return
+extension StarCardDTO {
+    var toCard: StarCard {
+        StarCard(
+            id: id,
+            nombre: nombre,
+            raza: raza,
+            descripcion: descripcion,
+            planetaOrigen: planetaOrigen,
+            epoca: epoca,
+            afiliacion: afiliacion.components(separatedBy: ", ").map(\.capitalized),
+            habilidades: habilidades.components(separatedBy: ", ").map(\.capitalized),
+            armas: armas.components(separatedBy: ", ").map(\.capitalized),
+            imagen: imagen
+        )
+    }
+}
+
+// What we want to return (used in presentation)
 // Identifiable: To enumerate elements
 // Hashable: Unique comparison value, two different elements (data - StarCard1 Vs StarCard2) with exact values inside their struct
 struct StarCard: Identifiable, Hashable {
