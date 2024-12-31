018f562b69ffb0cab8201b7992f2e45db7120c80
Oriol Angelet <uriaeri@gmail.com>
2024-12-31 16:35:29 +0100

    Test run

--- StarWarsSwift/StarWarsSwiftTests/StarWarsSwiftTests.swift
+++ StarWarsSwift/StarWarsSwiftTests/StarWarsSwiftTests.swift
@@ -16,10 +16,16 @@ extension Tag {
 struct StarWarsSwiftTests {
     
     let repository = RepositoryTest()
+    let viewModel = StarCardViewModel(repository: RepositoryTest())
     
     @Test("Prueba de carga de datos del repositorio")
     func dataLoad() throws {
         let data = try repository.getData()
         #expect(data.count == 4)
+    }
+    
+    @Test("Prueba de carga de datos del ViewModel")
+    func dataLoadViewModel() throws {
+        #expect(viewModel.cards.count == 4)
     }
 }
